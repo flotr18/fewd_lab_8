631e0f98da66e73a3d7f493b87d5c33156d15bb0
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-11-10 13:49:22 +0000

    Planet log added

---
+++ script.js
@@ -0,0 +1,8 @@
+let planets = ['Tatooine','Coruscant','Kamino','Mustafar','Kashyyyk'];
+
+console.log(planets[0]);
+console.log(planets[1]);
+console.log(planets[2]);
+console.log(planets[3]);
+console.log(planets[4]);
+
